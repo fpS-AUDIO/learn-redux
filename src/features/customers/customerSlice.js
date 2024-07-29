34a9b0b302d45178128e4dfba247889c59d11924
Alexander Ivanov <saudio.ru@gmail.com>
2024-07-29 21:34:12 +0200

    refactored to redux toolkit

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,45 +1,79 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const initialStateCustomer = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
-export default function CustomerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
+const customerSlice = createSlice({
+  name: `customer`,
+  initialState: initialStateCustomer,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
 
-    case "customer/updateName":
-      return {
-        ...state,
-        fullName: action.payload,
-      };
-
-    default:
-      return state;
-  }
-}
-
-// Action Function for Customer
-export function createCustomer(fullName, nationalID) {
-  return {
-    type: "customer/createCustomer",
-    payload: {
-      fullName: fullName,
-      nationalID: nationalID,
-      createdAt: new Date().toISOString(),
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      },
     },
-  };
-}
 
-export function updateName(fullName) {
-  return {
-    type: "customer/updateName",
-    payload: fullName,
-  };
-}
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+// export default function CustomerReducer(state = initialStateCustomer, action) {
+//   switch (action.type) {
+//     case "customer/createCustomer":
+//       return {
+//         ...state,
+//         fullName: action.payload.fullName,
+//         nationalID: action.payload.nationalID,
+//         createdAt: action.payload.createdAt,
+//       };
+
+//     case "customer/updateName":
+//       return {
+//         ...state,
+//         fullName: action.payload,
+//       };
+
+//     default:
+//       return state;
+//   }
+// }
+
+// // Action Function for Customer
+// export function createCustomer(fullName, nationalID) {
+//   return {
+//     type: "customer/createCustomer",
+//     payload: {
+//       fullName: fullName,
+//       nationalID: nationalID,
+//       createdAt: new Date().toISOString(),
+//     },
+//   };
+// }
+
+// export function updateName(fullName) {
+//   return {
+//     type: "customer/updateName",
+//     payload: fullName,
+//   };
+// }
--- src/store.js
+++ src/store.js
@@ -1,21 +1,24 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
 import AccountReducer from "./features/accounts/accountSlice";
 import CustomerReducer from "./features/customers/customerSlice";
-import { thunk } from "redux-thunk";
-// import { composeWithDevTools } from "@redux-devtools/extension/lib/types/logOnly";
-import { composeWithDevTools } from "@redux-devtools/extension";
 
-// ------ ROOT REDUCER -----
-const rootReducer = combineReducers({
-  account: AccountReducer,
-  customer: CustomerReducer,
+// import configureStore() instead of deprecated createStore()
+import { configureStore } from "@reduxjs/toolkit";
+
+/* ----- configureStore() automatically:
+- combine our reducers
+- add the Thunk middleware
+- set up the developer tools
+*/
+
+// configureStore accepta object of options
+// 'reducer' is the root reducer
+// 'reducer' should be object which tells RTK about the reducers
+
+const store = configureStore({
+  reducer: {
+    account: AccountReducer,
+    customer: CustomerReducer,
+  },
 });
 
-// to connect thunk to store you need, insdie the 'createStore' function,
-// specify the second argument: 'applyMiddleware' function (remember to import it)
-// 'applyMiddleware' accepts the middleware itself, in this case is 'thunk' (remember to import it
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
 export default store;
